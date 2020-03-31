652c890ee8c0e6f7c2a019a94dba60755e4f2b19
Peer Richelsen <founders@mage.market>
2020-03-31 22:58:18 +0200

    added firebase peer

---
+++ build/precache-manifest.7083a16019dc7463351bb245db70cf36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f64027d6f13e0b06ebd7c51456b13ab0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a56af1172cea2cf4677",
+    "url": "/static/css/2.c2bc211b.chunk.css"
+  },
+  {
+    "revision": "2a56af1172cea2cf4677",
+    "url": "/static/js/2.4a3da7fc.chunk.js"
+  },
+  {
+    "revision": "5865df2fd2d1111f74a20fe130c9e8c7",
+    "url": "/static/js/2.4a3da7fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e8e5b65d7a2e456200e",
+    "url": "/static/js/main.387ce613.chunk.js"
+  },
+  {
+    "revision": "ed2851cf6f9cfaf2c02e",
+    "url": "/static/js/runtime-main.1d09e445.js"
+  }
+]);
